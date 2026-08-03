7cca029bb7f0b809909ed799f9858925ec4b3331
Dante Insaurralde Viola <dantolin45@gmail.com>
2026-08-03 20:31:42 -0300

    Delete frontend directory

--- frontend/stocker/src/services/dashboardService.js
+++
@@ -1,6 +0,0 @@
-import { http } from "../lib/http";
-
-export async function getDashboardMetrics({ rangeDays = 30 } = {}) {
-  const { data } = await http.get("/dashboard", { params: { rangeDays } });
-  return data;
-}
